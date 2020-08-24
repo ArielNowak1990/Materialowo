2cd92b7b92c89a82e29f99e0757e968d5e2cb5cd
Ariel Nowak <ariel.nowak90@gmail.com>
2020-08-24 21:30:58 +0200

    carousel2

--- src/js/LandingPage_section4.js
+++ src/js/LandingPage_section4.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import Carousel2 from "./carousel2";
 
 function LandingPageSection3() {
  return (
      <div className="container">
-      <section className="popular">
-       <h2>Zamawialiśmy materiały dla:</h2>
-       <div className="proof_popular">
-
+      <section className="popular2">
+       <h2>Nie-Zamawialiśmy materiały dla:</h2>
+       <div className="proof_popular2">
+            <Carousel2/>
        </div>
       </section>
      </div>
---
+++ src/js/carousel2.js
@@ -0,0 +1,92 @@
+import React, {useState, useEffect} from 'react';
+import image1 from "../images/wakacje.jpg"
+import image2 from "../images/kurier.jpg"
+import image3 from "../images/pieniadze.jpg"
+import image4 from "../images/czas.jpg"
+import image5 from "../images/pieniadze.jpg"
+
+
+function Carousel2() {
+    const [counter, setCounter] = useState(0)
+
+
+    const carousel_element = () => {
+        if (counter === 0) {
+            return (
+                <>
+                <div><img src={image1} alt="wakacje"/></div>
+                <div><img src={image2} alt="wakacje"/></div>
+                <div><img src={image3} alt="wakacje"/></div>
+                <div><img src={image4} alt="wakacje"/></div>
+
+                </>
+                )
+        }
+        if (counter === 1) {
+            return (
+                    <>
+                        <div><img src={image2} alt="wakacje"/></div>
+                        <div><img src={image3} alt="wakacje"/></div>
+                        <div><img src={image4} alt="wakacje"/></div>
+                        <div><img src={image5} alt="wakacje"/></div>
+                        </>
+                )
+        }
+        if (counter === 2) {
+            return (
+                <>
+                    <div><img src={image3} alt="wakacje"/></div>
+                    <div><img src={image4} alt="wakacje"/></div>
+                    <div><img src={image5} alt="wakacje"/></div>
+                    <div><img src={image1} alt="wakacje"/></div>
+                    </>
+            )
+        }
+        if (counter === 3) {
+            return (
+                <>
+                    <div><img src={image4} alt="wakacje"/></div>
+                    <div><img src={image5} alt="wakacje"/></div>
+                    <div><img src={image1} alt="wakacje"/></div>
+                    <div><img src={image2} alt="wakacje"/></div>
+                </>
+            )
+        }
+    };
+
+
+    useEffect(() => {
+            const timeOut = setTimeout(() => {
+                if (counter === 3) {setCounter(0)}
+                else {setCounter(prevState => prevState + 1)}
+
+                },2500
+            );
+            return() => {clearTimeout(timeOut);
+        }
+
+        },[counter]
+    )
+
+    const handlePrev = () => {
+        if (counter === 0) {setCounter(3)}
+        else {setCounter(prevState => prevState - 1)}
+    }
+
+    const handleNext = () => {
+        if (counter === 3) {setCounter(0)}
+        else {setCounter(prevState => prevState + 1)}
+    }
+
+
+    return (
+        <div className={"carousel2"}>
+            <button onClick={handlePrev}>&lt;</button>
+            <p> {carousel_element()}</p>
+            <button onClick={handleNext}>&gt;</button>
+
+        </div>
+    );
+}
+
+export default Carousel2;
