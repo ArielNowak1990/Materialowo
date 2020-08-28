c6c01b4712b956fd1554d21619437e48905798e1
Ariel Nowak <ariel.nowak90@gmail.com>
2020-08-28 19:38:26 +0200

    section 3 - visual

--- src/js/LandingPage/LandingPage_section3.js
+++ src/js/LandingPage/LandingPage_section3.js
@@ -8,29 +8,29 @@ function LandingPageSection3() {
        <h2>Dlatego warto skorzystać z naszych usług:</h2>
        <div className="proof_popular">
         <div className="box">
-         <div><img src="images/Person.svg" alt=""/></div>
-         <h3>100000+</h3>
-         <p>daily entries</p>
+         <div><i className="far fa-clock"></i></div>
+         <h3>90 min</h3>
+         <p>Tyle oszczędzisz na jednej karcie</p>
         </div>
         <div className="box">
-         <div><img src="images/Eye.svg" alt=""/></div>
-         <h3>2000000+</h3>
-         <p>seen products every day</p>
+         <div><i className="fas fa-coins"></i></div>
+         <h3>15%</h3>
+         <p>Tyle średnio tańsze zakupy</p>
         </div>
         <div className="box">
-         <div><img src="images/Catalog.svg" alt=""/></div>
-         <h3>10000+</h3>
-         <p>added post daily</p>
+         <div><i className="fas fa-money-bill-alt"></i></div>
+         <h3>37%</h3>
+         <p>Tyle najczęściej się oszczędza</p>
         </div>
         <div className="box">
-         <div><img src="images/Catalog.svg" alt=""/></div>
-         <h3>10000+</h3>
-         <p>added post daily</p>
+         <div><i className="fas fa-laugh-wink"></i></div>
+         <h3>+5</h3>
+         <p>tyle zyskujesz do samopoczucia</p>
         </div>
         <div className="box">
-         <div><img src="images/Catalog.svg" alt=""/></div>
-         <h3>10000+</h3>
-         <p>added post daily</p>
+         <div><i className="fas fa-thumbs-up"></i></div>
+         <h3>uznanie</h3>
+         <p>od szefa za ładne dokumenty</p>
         </div>
        </div>
       </section>
