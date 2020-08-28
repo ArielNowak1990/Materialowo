1ea22937f4341c6f4a146f53520b560b547ee5ab
Ariel Nowak <ariel.nowak90@gmail.com>
2020-08-29 01:04:47 +0200

    history orders

--- src/js/App/ActualForm.js
+++ src/js/App/ActualForm.js
@@ -25,39 +25,35 @@ function ActualForm(user) {
     if (orders.length >=1) {
     return (
         <section className={"container"}>
-            <div className={"pulpit_background"}>
-                <div className={"box"}>
-                    <div className={"recipes"}>
-                        <div className={"recipes_header"}>
+                    <div className={"list"}>
+                        <div className={"list_header"}>
                             <h1>LISTA WYSŁANYCH ZAMÓWIEŃ:</h1>
-                            <Link to={"#"}><i className="fas fa-plus-square"/></Link>
+                            <Link to={"/app/NewForm"}><i className="fas fa-plus-square"/></Link>
                         </div>
-                        <div className={"recipes_table"}>
+                        <div className={"list_table"}>
                             <div className={"table_header"}>
                                 <div className={"id"}>ID</div>
-                                <div className={"nazwa"}>ODBIORCY:</div>
-                                <div className={"opis"}>MATERIAŁY</div>
+                                <div className={"odbiorcy"}>ODBIORCY:</div>
+                                <div className={"term"}>ZAKONCZENIE</div>
+                                <div className={"materialy"}>MATERIAŁY</div>
                                 <div className={"akcje"}>AKCJE</div>
                             </div>
                             <div className={"table"}>
                                 {orders.map((element, index) => {return(
                                     <div key={element.id} className={"table_element"}>
                                         <div className={"id"}>{element.id}</div>
-                                        <div className={"nazwa"}>{element.firm}</div>
-                                        <div className={"nazwa"}>{element.status}</div>
-                                        {/* TODO tu konieczna druga pętla*/}
-                                        <div className={"nazwa"}>{element.elements.fabric}</div>
-                                        <div className={"nazwa"}>{element.dateAuthor[2]}</div>
+                                        <div className={"odbiorcy"}><ol>
+                                            {element.firm.map( (el, ind) =>{return ( <li key={ind}>{ind+1} {el}</li>)} )}</ol></div>
+                                        <div className={"term"}>{element.dateAuthor[1]}</div>
+                                        <div className={"materialy"}><ol>{element.elements.map( (ele, inde) => {return(<li key={inde}>{inde+1} {ele.fabric} {ele.unit} {ele.quantity} </li>)})}</ol></div>
+
                                     </div>
                                 )})}
                             </div>
                         </div>
-
                     </div>
-                </div>
-            </div>
         </section>
-    )};
+    )}
 }
 
 export default ActualForm;
--- src/js/App/HistoryForm.js
+++ src/js/App/HistoryForm.js
@@ -23,41 +23,37 @@ function HistoryForm(user) {
 
     if (orders.length < 1) {return ( "trwa wczytywanie danych")}
     if (orders.length >=1) {
-    return (
-        <section className={"container"}>
-            <div className={"pulpit_background"}>
-                <div className={"box"}>
-                    <div className={"recipes"}>
-                        <div className={"recipes_header"}>
-                            <h1>LISTA WYSŁANYCH I OCZEKUJĄCYCH ZAMÓWIEŃ</h1>
-                            <Link to={"#"}><i className="fas fa-plus-square"/></Link>
+        return (
+            <section className={"container"}>
+                <div className={"list"}>
+                    <div className={"list_header"}>
+                        <h1>LISTA WYSŁANYCH ZAMÓWIEŃ:</h1>
+                        <Link to={"/app/NewForm"}><i className="fas fa-plus-square"/></Link>
+                    </div>
+                    <div className={"list_table"}>
+                        <div className={"table_header"}>
+                            <div className={"id"}>ID</div>
+                            <div className={"odbiorcy"}>ODBIORCY:</div>
+                            <div className={"term"}>ZAKONCZENIE</div>
+                            <div className={"materialy"}>MATERIAŁY</div>
+                            <div className={"akcje"}>AKCJE</div>
                         </div>
-                        <div className={"recipes_table"}>
-                            <div className={"table_header"}>
-                                <div className={"id"}>ID</div>
-                                <div className={"nazwa"}>ODBIORCY:</div>
-                                <div className={"opis"}>MATERIAŁY</div>
-                                <div className={"akcje"}>AKCJE</div>
-                            </div>
-                            <div className={"table"}>
-                                {orders.map((element, index) => {return(
-                                    <div key={element.id} className={"table_element"}>
-                                        <div className={"id"}>{element.id}</div>
-                                        <div className={"nazwa"}>{element.firm}</div>
-                                        <div className={"nazwa"}>{element.status}</div>
-                                        {/* TODO tu konieczna druga pętla*/}
-                                        <div className={"nazwa"}>{element.elements.fabric}</div>
-                                        <div className={"nazwa"}>{element.dateAuthor[2]}</div>
-                                    </div>
-                                )})}
-                            </div>
-                        </div>
+                        <div className={"table"}>
+                            {orders.map((element, index) => {return(
+                                <div key={element.id} className={"table_element"}>
+                                    <div className={"id"}>{element.id}</div>
+                                    <div className={"odbiorcy"}><ol>
+                                        {element.firm.map( (el, ind) =>{return ( <li key={ind}>{ind+1} {el}</li>)} )}</ol></div>
+                                    <div className={"term"}>{element.dateAuthor[1]}</div>
+                                    <div className={"materialy"}><ol>{element.elements.map( (ele, inde) => {return(<li key={inde}>{inde+1} {ele.fabric} {ele.unit} {ele.quantity} </li>)})}</ol></div>
 
+                                </div>
+                            )})}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </section>
-    )};
+            </section>
+        )}
 }
 
 export default HistoryForm;
