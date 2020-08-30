c89f7e23350cd708b8c91b6eecbf0aa4a6f336a7
Ariel Nowak <ariel.nowak90@gmail.com>
2020-08-30 19:52:06 +0200

    first tabel in CardChoice

--- src/js/App/NewCardChoice.js
+++ src/js/App/NewCardChoice.js
@@ -31,15 +31,61 @@ function NewCardChoice() {
 
 if (!allOfert){return ("wczytywanie")}
 if (allOfert){
+    const style= {
+        rowspan: 3
+    }
     return (
-        <section className={"container"}>
-            <div className={"form"}>
-                <h3>Karta wyboru do zamówienia ............. </h3>
-                <div className={"NewOrder"}>
-                    {allOfert.map( (element) => {return(
-                        <p>{element.id}</p>
-                    )})}
-                </div>
+        <section className={"container_Choice"}>
+            <div className={"cardChoice"}>
+                <div className={"wycena"}>
+                    <h4>1. Tabela z elementami do wyceny</h4>
+                    <table>
+                        <thead>
+                        <tr>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            <td> </td>
+                            {allOfert[0].firm.map((element, index) => {return(
+                                <td colSpan={3} key={index}>{element}</td>
+                            )})}
+                        </tr>
+                        <tr>
+                            <td>Lp.</td>
+                            <td>Materiał</td>
+                            <td>Miara</td>
+                            <td>Ilość</td>
+                            <td>Uwagi Zamawiającego</td>
+                            {allOfert[0].firm.map((element, index) => {return(
+                               <>
+                                <td key={index}>Cena J</td>
+                                <td>Cena C</td>
+                                <td>Uwagi Dostawcy</td>
+                                </>
+                            )})}
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {allOfert[0].elements.map((element,index)=>{
+                            return(
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{element.fabric}</td>
+                                    <td>{element.unit}</td>
+                                    <td>{element.quantity}</td>
+                                    <td>{element.note}</td>
+                                    <td>{element.price}</td>
+                                    <td>{element.priceAll}</td>
+                                    <td>{element.noteFirm}</td>
+                                </tr>
+                            )
+                        })}
+
+                        </tbody>
+                    </table>
+                    </div>
             </div>
         </section>
     )}
