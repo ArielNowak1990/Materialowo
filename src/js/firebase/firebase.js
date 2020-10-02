f2f7893ebddea7b602ea11682a38560f324c49d8
Ariel Nowak <ariel.nowak90@gmail.com>
2020-10-02 18:47:24 +0200

    firebase.js

---
+++ src/js/firebase/firebase.js
@@ -0,0 +1,35 @@
+import app from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
+// import React from "react";
+import {configuration} from "../../pass";
+
+const config = configuration;
+
+
+class Firebase {
+    constructor() {
+        app.initializeApp(config);
+
+        this.auth = app.auth();
+        this.db = app.database();
+    }
+
+    //Auth API
+
+    doCreateUserWithEmailAndPassword = (email, password) =>
+        this.auth.createUserWithEmailAndPassword(email, password);
+
+    doSignInWithEmailAndPassword = (email, password) =>
+        this.auth.signInWithEmailAndPassword(email, password);
+
+    doSignOut = () => this.auth.signOut();
+
+    //User API
+
+    user = uid => this.db.ref(`users/${uid}`);
+    users = () => this.db.ref('users');
+
+}
+
+export default Firebase;
